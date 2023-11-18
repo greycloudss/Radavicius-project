cd2b00608f78f45ca90567ac80d8c633230671ce
Dovydas Levinas <dovydas.levinas@mif.stud.vu.lt>
2023-11-18 16:26:55 +0200

    fixed balance branching into transfers; ui is fully functional now; new broken boi = login func always passes as false. or crashes on fname input

--- banking_ui.c
+++ banking_ui.c
@@ -3,61 +3,85 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <stdbool.h>
+#include <string.h>
 #define u_char unsigned char
 
 typedef struct {
 	char fname[30];
-	char email[30];
 	char user_id[10];
-	char phone[20];
 	char pword[20];
 } info;
 
+bool regist(bool status) {
+	while (1) {
+		
+	}
 
 
-bool login(info* x) {
+	return status;
+}
+
+bool login() {
 	system("cls");
-	FILE* cache;
-	cache = fopen("database.bin", "rb+");
-	bool flag = false;
-	char username[30], password[30];
-	printf("Enter username: ");
-	fgets(username, 30, stdin);
-	printf("Enter password: ");
-	fgets(password, 30, stdin);
-	username[strcspn(username, "\n")] = 0; // remove trailing newline
-	password[strcspn(password, "\n")] = 0; // remove trailing newline
-	info user;
-	while (fread(&user, sizeof(info), 1, cache)) {
-
-		if (strcmp(username, user.user_id) == 0 && strcmp(password, user.pword) == 0) {
-			printf("Login successful\n");
-			flag = true;
-			break;
-
+	FILE* cache = fopen("database.txt", "r");
+	bool flags[3] = { false, false, false };
+	char prompts[3][30] = { "enter your fname : ", "enter user id: ", "enter your password: " };
+	char tmp[3][30] = { 0 };
+	int sizers[3] = { 30, 10, 20 };
+	u_char count = 0;
+	while (count < 3) {
+		printf("%s", prompts[count]);
+		
+		if (scanf("%29s", &tmp[count]) != 0) {
+			// Remove the newline character from the input
+			tmp[count][strcspn(tmp[count], "\n")] = 0;
+			if (sizeof(tmp[count]) == sizers[count]) {
+				char line[100];
+				rewind(cache); // Reset the file pointer to the beginning of the file
+				while (fgets(line, sizeof(line), cache) != NULL) {
+					// Remove the newline character from the line
+					line[strcspn(line, "\n")] = 0; 
+					if (strcmp(line, tmp[count]) == 0) {
+						flags[count++] = true;
+						system("cls");
+						break;
+					}
+				}
+			}
+			else
+				printf("Input too long, must be %d characters long\n", sizers[count]);
+		}
+		else {
+			printf("invalid input");
+			while (getchar() != '\n')
+				;
 		}
 	}
-	printf("Incorrect credentials\n");
 	fclose(cache);
-	return flag;
+	for (int i = 0; i < 3; i++) {
+		if (flags[i] == false)
+			return false;
+	}
+	return true;
 }
 
 char prompt(u_char instance, bool status) {
 	char sel[][30] = { {"Login"}, {"Register"}, {"Transfer"}, {"Balance"}, {"Local transfer"}, {"Extern transfer" } }; // selection names
-	u_char sel_codes[][2] = { {0, 1}, {2, 3}, {4, 5} }; /*{"Transfer"}, {"Balance"}, {""}};*/ // selection codes
+	u_char sel_codes[][2] = { {0, 1}, {3, 2}, {4, 5} }; /*{"Transfer"}, {"Balance"}, {""}};*/ // selection codes
 	// if this was lua or py this would be easier to do by just saying do x in pairs y;
 
 	u_int count = 0; // basically max selections available on screen so every time enter is pressed count goes up
 
 	bool state = true; //state if the selection changes
 	do {
+		if (status == false && instance >= 1)
+			return 0;
 		if (GetAsyncKeyState(VK_UP) || GetAsyncKeyState(VK_DOWN)) {
 			count = count ^ 1;
 			state = true;
 		}
 		if (state == true) {
 			system("cls");
-			printf("--%d--", instance);
 			printf("[=--> %s <--=]\n(-- %s --)", sel[sel_codes[instance][count^1]], sel[sel_codes[instance][count]]);
 
 			state = false;
@@ -91,8 +115,9 @@ void main() {
 	bool status = false;
 	while (1) {
 		if (1 == instance) {
-			status = login(&x);
-
+			status = login();
+			if (status == false)
+				instance = 0;
 		}
 		bonk = prompt(instance, status);
 		instance += bonk;
